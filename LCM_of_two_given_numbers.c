fea6034a747b638ce38222eb2991886580374e38
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2022-12-13 17:56:13 +0530

    #include<stdio.h> int main() {     int l,u,i;     scanf("%d%d",&l,&u);     for(i=1;i<=u;i++)     {         if((l*i)%u==0)         {             printf("%d",l*i);             break;
            }
        }
    }

---
+++ LCM_of_two_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int l,u,i;
+    scanf("%d%d",&l,&u);
+    for(i=1;i<=u;i++)
+    {
+        if((l*i)%u==0)
+        {
+            printf("%d",l*i);
+            break;
+            
+        }
+    }
+}
