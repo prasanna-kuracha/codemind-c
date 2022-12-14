a562eaab1915fb695868cc4d824b596d5f25f456
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2022-12-14 18:59:23 +0530

    #include<stdio.h> int main() {     int i,n,r=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(i%2!=0)         {             printf("%d ",i);         }     }
    }

--- Odd Natural_Numbers.c
+++ Odd Natural_Numbers.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int i,n,r=0;
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
         if(i%2!=0)
         {
             printf("%d ",i);
         }
     }
+    
 }
