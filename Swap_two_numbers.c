f5ee18596b3a5b6db0ff2dc39a00ea4cb1d4b7fb
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2022-12-29 23:21:04 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     printf("%d %d",b,a); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -4,22 +4,5 @@ int main()
     int a,b;
     scanf("%d%d",&a,&b);
     printf("%d
-%d",b,a);#include<stdio.h>
-int main()
-{
-    int n,a[n],i,s=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2==0)
-        {
-            s=s+a[i];
-        }
-    }
-    printf("%d",s);
-}
+%d",b,a);
 }
