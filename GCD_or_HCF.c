fe8821e747e00d5543155c57c908d7d1c1f5bb30
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2022-12-30 14:33:04 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,g,i;     scanf("%d%d",&a,&b);     for(i=1;i<=a && i<=b;i++)     {         if(a%i==0 && b%i==0)         g=i;     }      printf("%d",g); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,g,i;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a && i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        g=i;
+    }
+     printf("%d",g);
+}
