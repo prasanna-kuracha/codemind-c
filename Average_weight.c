a909fa1666faa98f61a0ecf99670ff2ea834f81c
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-01-06 22:05:02 +0530

    #include<stdio.h> int main() {     int x,a,b,c;     scanf("%d%d%d",&a,&b,&x);     c=3*x-a-b;     printf("%d",c); }

--- Average_weight.c
+++ Average_weight.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
-     int x=3*c-a-b;
-    printf("%d",x);
+    int x,a,b,c;
+    scanf("%d%d%d",&a,&b,&x);
+    c=3*x-a-b;
+    printf("%d",c);
 }
