c923d71725c962909a2b201c536229edf88c9bf7
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-01-09 23:32:44 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n-i+1;j++)
+        for(j=1;j<=i;j++)
         {
             printf("%d",j);
         }
