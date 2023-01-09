5c305cc624b172e2f10e0c8debf55d9f969c5a8f
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-01-09 22:06:05 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             if(i%2!=0)             {                 c++;             }             else             {                 c=0;                 break;             }         }     }     if(c>0)//c++     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            if(i%2!=0)
+            {
+                c++;
+            }
+            else
+            {
+                c=0;
+                break;
+            }
+        }
+    }
+    if(c>0)//c++
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
