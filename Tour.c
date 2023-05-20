abaaaed7f669ede48ed731934222a5596bcfd95f
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-05-20 11:20:26 +0530

    #include<stdio.h> int main() {   int i,n;   scanf("%d",&n);   for(i=1;i<=n;i++)   {       int a,b;       scanf("%d%d",&a,&b);       int k=(a*5+b*7);       printf("%d ",k);
      }
    }

---
+++ Tour.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int i,n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      int a,b;
+      scanf("%d%d",&a,&b);
+      int k=(a*5+b*7);
+      printf("%d
+",k);
+      
+  }
+}
