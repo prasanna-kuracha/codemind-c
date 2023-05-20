03ce9c4acfb314aa114d437fa22722aa96cfa584
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-05-20 11:21:38 +0530

    #include<stdio.h> int main() {   int i,n;   scanf("%d",&n);   for(i=1;i<=n;i++)   {       int x;       scanf("%d",&x);       if(x>30){           printf("YES ");       }       else       {           printf("NO ");       }   } }

---
+++ Subscription.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int i,n;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      int x;
+      scanf("%d",&x);
+      if(x>30){
+          printf("YES
+");
+      }
+      else
+      {
+          printf("NO
+");
+      }
+  }
+}
