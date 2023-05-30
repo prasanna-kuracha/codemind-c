395682df995e025282baab8bc9dd5fa909f3972a
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-05-30 15:56:37 +0530

    #include<stdio.h> int fun(int n) {     int i;     for(i=2;i<=n/2;i++)     {         if(n%i==0) return 0;     }     return 1; } int main() {     int n,s,c=0,y,i;     scanf("%d",&n);     for(i=2;i<n;i++)     {         if(n%i==0)         {             s=i;             if(fun(s)==1)             {                 y=n/s;                 if(fun(y)==1)                 {                     c=1;                     printf("%d %d",s,y);                     break;                 }             }         }     }     if(c==0) printf("-1"); }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -4,29 +4,26 @@ int fun(int n)
     int i;
     for(i=2;i<=n/2;i++)
     {
-        if(n%i==0)
-        {
-            return 0;
-        }
+        if(n%i==0) return 0;
     }
     return 1;
 }
 int main()
 {
-    int n,i,s,m,c=0;
+    int n,s,c=0,y,i;
     scanf("%d",&n);
-    for(i=2;i<n/2;i++)
+    for(i=2;i<n;i++)
     {
         if(n%i==0)
         {
             s=i;
             if(fun(s)==1)
             {
-                m=n/s;
-                if(fun(m)==1)
+                y=n/s;
+                if(fun(y)==1)
                 {
                     c=1;
-                    printf("%d %d",s,m);
+                    printf("%d %d",s,y);
                     break;
                 }
             }
