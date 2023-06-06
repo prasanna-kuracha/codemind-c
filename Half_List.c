6911684c8ba993de82046ca8a3850059270ccfac
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-06-06 22:51:05 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],b[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m=n/2,j=0;     for(i=n-1;i>=m;i--)     {         b[j]=a[i];         j++;     }     for(i=0;i<m;i++)     {         b[j]=a[i];         j++;     }     for(i=0;i<n;i++)     {         printf("%d ",b[i]);     }
    }

---
+++ Half_List.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int m=n/2,j=0;
+    for(i=n-1;i>=m;i--)
+    {
+        b[j]=a[i];
+        j++;
+    }
+    for(i=0;i<m;i++)
+    {
+        b[j]=a[i];
+        j++;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",b[i]);
+    }
+    
+}
