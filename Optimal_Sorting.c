e26f1abdb67f87e5f944998a1a3e5f6bd5781c61
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-07-08 15:38:33 +0530

    #include<stdio.h> int main() {     int n,k;     scanf("%d",&n);     for(k=0;k<n;k++)     {         int h,i,j,k;         scanf("%d",&h);         int a[h];         for(i=0;i<h;i++)         {             scanf("%d",&a[i]);         }         for(i=0;i<h-1;i++)         {             if(a[i]>a[i+1])             {                 k=1;                 break;             }         }         if(k==0) printf("0");         else         {             int min=200,max=0;             for(i=0;i<h;i++)             {                 if (a[i]>max) max=a[i];                 if(a[i]<min) min=a[i];             }             printf("%d ",max-min);         }     } }

--- Optimal_Sorting.c
+++ Optimal_Sorting.c
@@ -1,50 +1,36 @@
 #include<stdio.h>
-int fun(int *a,int m)
-{
-    int j;
-    for(j=0;j<m-1;j++)
-    {
-        if(a[j]>a[j+1])
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
 int main()
 {
     int n,k;
     scanf("%d",&n);
-    for(k=1;k<=n;k++)
+    for(k=0;k<n;k++)
     {
-        int m,i;
-        scanf("%d",&m);
-        int a[m];
-        for(i=0;i<m;i++)
+        int h,i,j,k;
+        scanf("%d",&h);
+        int a[h];
+        for(i=0;i<h;i++)
         {
             scanf("%d",&a[i]);
         }
-        if (fun(a,m)==1)
+        for(i=0;i<h-1;i++)
         {
-            printf("0");
-        }
-        else{
-            int max=0 ,min=100;
-            for(i=0;i<m;i++)
+            if(a[i]>a[i+1])
             {
-                if(a[i]>max)
-                {
-                    max=a[i];
-                }
-                if(a[i]<min)
-                {
-                    min=a[i];
-                }
+                k=1;
+                break;
+            }
+        }
+        if(k==0) printf("0");
+        else
+        {
+            int min=200,max=0;
+            for(i=0;i<h;i++)
+            {
+                if (a[i]>max) max=a[i];
+                if(a[i]<min) min=a[i];
             }
             printf("%d
 ",max-min);
         }
     }
-    
-    
 }
