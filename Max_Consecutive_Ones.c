cfcf45aba37177d1b47d6c78ddfffcb6683c011e
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2023-07-08 16:38:04 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c,max=0;     for(i=0;i<n;i++)     {         if(a[i]==1)         {             c=0;             for(j=i;j<n;j++)             {                 if(a[i]==a[j])                 {                     c++;                 }                 else                 {                     break;                 }             }         }         if(c>max) max=c;     }     printf("%d",max); }

---
+++ Max_Consecutive_Ones.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c,max=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==1)
+        {
+            c=0;
+            for(j=i;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if(c>max) max=c;
+    }
+    printf("%d",max);
+}
